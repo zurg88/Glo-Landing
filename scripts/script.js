f21ed9c95e599bd012b0594ff63a9c8314b5a387
Sergey <gnatenko@gmail.com>
2020-07-03 23:37:53 +0300

    lesson19 fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,55 +1,57 @@
+/* eslint-disable no-empty */
+/* eslint-disable no-mixed-spaces-and-tabs */
 
 
 
 window.addEventListener('DOMContentLoaded', () => {
 
-	// function countTimer(daedline) {
-	// 	const timerhHours = document.querySelector('#timer-hours'),
-	// 		timermMinutes = document.querySelector('#timer-minutes'),
-	// 		timersSeconds = document.querySelector('#timer-seconds');
+	function countTimer(daedline) {
+ 	const timerhHours = document.querySelector('#timer-hours'),
+ 		timermMinutes = document.querySelector('#timer-minutes'),
+ 		timersSeconds = document.querySelector('#timer-seconds');
 
-	// 	function getTimeRemaining() {
-	// 		const dateStop = new Date(daedline).getTime(),
-	// 			dateNow = new Date().getTime(),
-	// 			timeRemaining = (dateStop - dateNow) / 1000,
-	// 			seconds = Math.floor(timeRemaining % 60),
-	// 			minutes = Math.floor((timeRemaining / 60) % 60),
-	// 			hours = Math.floor(timeRemaining / 60 / 60);
-	// 		return { timeRemaining, hours, minutes, seconds };
-	// 	}
+ 	function getTimeRemaining() {
+ 		const dateStop = new Date(daedline).getTime(),
+ 			dateNow = new Date().getTime(),
+ 			timeRemaining = (dateStop - dateNow) / 1000,
+ 			seconds = Math.floor(timeRemaining % 60),
+ 			minutes = Math.floor((timeRemaining / 60) % 60),
+ 			hours = Math.floor(timeRemaining / 60 / 60);
+ 		return { timeRemaining, hours, minutes, seconds };
+ 	}
 
-	// 	function updateClock() {
-	// 		const timer = getTimeRemaining();
+ 	function updateClock() {
+ 		const timer = getTimeRemaining();
 
-	// 		timerhHours.textContent = timer.hours;
-	// 		timermMinutes.textContent = timer.minutes;
-	// 		timersSeconds.textContent = timer.seconds;
+ 		timerhHours.textContent = timer.hours;
+ 		timermMinutes.textContent = timer.minutes;
+ 		timersSeconds.textContent = timer.seconds;
 
-	// 		if (timer.hours < 10) {
-	// 			timerhHours.textContent = '0' + timer.hours;
-	// 		} else if (timer.minutes < 10) {
-	// 			timermMinutes.textContent = '0' + timer.minutes;
-	// 		} else if (timer.seconds < 10) {
-	// 			timersSeconds.textContent = '0' + timer.seconds;
-	// 		}
+ 		if (timer.hours < 10) {
+ 			timerhHours.textContent = '0' + timer.hours;
+ 		} else if (timer.minutes < 10) {
+ 			timermMinutes.textContent = '0' + timer.minutes;
+ 		} else if (timer.seconds < 10) {
+ 			timersSeconds.textContent = '0' + timer.seconds;
+ 		}
 
-	// 		if (timer.timeRemaining < 0) {
-	// 			console.log(timerhHours);
-	// 			timerhHours.textContent = '00';
-	// 			timermMinutes.textContent = '00';
-	// 			timersSeconds.textContent = '00';
-	// 		}
+ 		if (timer.timeRemaining < 0) {
+ 			console.log(timerhHours);
+ 			timerhHours.textContent = '00';
+ 			timermMinutes.textContent = '00';
+ 			timersSeconds.textContent = '00';
+ 		}
 
-	// 		if (timer.timeRemaining > 0) {
-	// 			setInterval(updateClock, 1000);
-	// 		}
-	// 		//
-	// 	}
-	// 	updateClock();
+ 		if (timer.timeRemaining > 0) {
+				setTimeout(updateClock, 1000);
+ 		}
 
-	// }
+		}
+		updateClock();
 
-	// countTimer('03 July 2020');
+	}
+
+	countTimer('05 July 2020');
 
 	// Menu
 
@@ -78,30 +80,47 @@ window.addEventListener('DOMContentLoaded', () => {
 		const popup = document.querySelector('.popup');
 		const popupClose = document.querySelectorAll('.popup-close');
 		const popupBtn = document.querySelectorAll('.popup-btn');
-		const popupContent = document.querySelector('.popup-content');
 
 		popupBtn.forEach(item => {
 			item.addEventListener('click', () => {
-				popupContent.style.top = '-110%';
-				let count = -110;
-				const popupShowAnimation = requestAnimationFrame(function popupAnimation()  {
-					count +=+ 4;
-					popupContent.style.top = count + '%';
-					if (count < 10) requestAnimationFrame(popupAnimation);
-					console.dir(document.documentElement.clientWidth);
-				});
-				if (document.documentElement.clientWidth < 768) {
-					cancelAnimationFrame(popupShowAnimation);
-					popupContent.style.top = '10%';
+				if (document.documentElement.clientWidth > 768) {
+					showPopupAnimation();
 				}
 
 				popup.style.display = 'block';
 			});
 		});
 
+		function showPopupAnimation() {
+			const popupContent = document.querySelector('.popup-content');
+			popupContent.style.top = '-110%';
+			let count = -110;
+			requestAnimationFrame(function popupAnimation()  {
+				count += +4;
+				popupContent.style.top = count + '%';
+				popup.style.top = 0 + '%';
+				if (count < 10) requestAnimationFrame(popupAnimation);
+			});
+
+		}
+
+		function closePopupAnimation() {
+			const popupContent = document.querySelector('.popup-content');
+			let count1 = 10;
+			let count2 = 10;
+			requestAnimationFrame(function popupAnimation()  {
+				count1 -= 10;
+				count2 -= 4;
+				popup.style.top = count1 + '%';
+				popupContent.style.top = count2 + '%';
+				if (count2 > -110) requestAnimationFrame(popupAnimation);
+				// popup.style.display = 'none';
+			});
+		}
+
 		popupClose.forEach(item => {
 			item.addEventListener('click', () => {
-				popup.style.display = 'none';
+				closePopupAnimation();
 			});
 		});
 	};
