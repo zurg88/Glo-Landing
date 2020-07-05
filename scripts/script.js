191683fccea258e9e169f718c6587658c55c6caf
Sergey <gnatenko@gmail.com>
2020-07-05 23:06:49 +0300

    lesson 21 homework

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 
 window.addEventListener('DOMContentLoaded', () => {
+	//Timer
 
 	function countTimer(daedline) {
  	const timerhHours = document.querySelector('#timer-hours'),
@@ -34,7 +35,6 @@ window.addEventListener('DOMContentLoaded', () => {
  		}
 
  		if (timer.timeRemaining < 0) {
- 			console.log(timerhHours);
  			timerhHours.textContent = '00';
  			timermMinutes.textContent = '00';
  			timersSeconds.textContent = '00';
@@ -66,7 +66,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
 		document.addEventListener('click', event => {
 			const target = event.target;
-			console.log(target);
 			if (target.closest('.menu')) {
 				handlerMenu();
 			} else {
@@ -184,5 +183,125 @@ window.addEventListener('DOMContentLoaded', () => {
 	};
 
 	tabs();
+
+	// Slider
+
+	const slider = () => {
+		const slide = document.querySelectorAll('.portfolio-item'),
+		 btn = document.querySelectorAll('.portfolio-btn'),
+		 slider = document.querySelector('.portfolio-content'),
+		 portfolioDots = document.querySelector('.portfolio-dots');
+
+		 let currentSlide = 0;
+		 let interval;
+
+		//  const createElem = (strElem, ClassStr) => {
+		// 	 let element = document.createElement(strElem);
+		// 	element.classList.add(ClassStr);
+		//  };
+
+
+		function createDots() {
+
+			const dotsCollection = portfolioDots.querySelectorAll('.dot');
+			const dotElement = document.createElement('li');
+			dotElement.classList.add('dot');
+			portfolioDots.append(dotElement);
+
+			if (dotsCollection.length < slide.length) {
+				createDots();
+			} else if (dotsCollection.length === slide.length) {
+				dotsCollection[0].classList.add('dot-active');
+				return dotsCollection;
+			}
+
+		 }
+
+		 createDots();
+		 
+		 let dot = portfolioDots.querySelectorAll('.dot');
+
+		 console.log(dot);
+
+
+		 const prevSlide = (elem, index, strClass) => {
+			elem[index].classList.remove(strClass);
+		 };
+
+		 const nextSlide = (elem, index, strClass) => {
+			elem[index].classList.add(strClass);
+		 };
+
+
+		 const autoPlaySlide = () => {
+			prevSlide(slide, currentSlide, 'portfolio-item-active');
+			prevSlide(dot, currentSlide, 'dot-active');
+			currentSlide++;
+			if (currentSlide >= slide.length) {
+				currentSlide = 0;
+			}
+			nextSlide(slide, currentSlide, 'portfolio-item-active');
+			nextSlide(dot, currentSlide, 'dot-active');
+		 };
+
+		 const startSlide = (time = 2500) => {
+			interval = setInterval(autoPlaySlide, time);
+		 };
+
+		 const stoptSlide = () => {
+			clearInterval(interval);
+		 };
+
+		 slider.addEventListener('click', event => {
+			event.preventDefault();
+
+			const target = event.target;
+
+			if (!target.matches('.portfolio-btn, .dot')) {
+				return;
+			}
+
+			prevSlide(slide, currentSlide, 'portfolio-item-active');
+			prevSlide(dot, currentSlide, 'dot-active');
+
+			if (target.matches('#arrow-right')) {
+				currentSlide++;
+			} else if (target.matches('#arrow-left')) {
+				currentSlide--;
+			} else if (target.matches('.dot')) {
+				dot.forEach((elem, index) => {
+					if (target === elem) {
+						currentSlide = index;
+					}
+				});
+			}
+
+			if (currentSlide >= slide.length) {
+				currentSlide = 0;
+			} else if (currentSlide < 0) {
+				currentSlide = slide.length - 1;
+			}
+
+			nextSlide(slide, currentSlide, 'portfolio-item-active');
+			nextSlide(dot, currentSlide, 'dot-active');
+		 });
+
+		 slider.addEventListener('mouseover', event => {
+			 if (event.target.matches('.portfolio-btn') || event.target.matches('.dot')) {
+				 stoptSlide();
+			 }
+
+		 });
+		 slider.addEventListener('mouseout', event => {
+			if (event.target.matches('.portfolio-btn') || event.target.matches('.dot')) {
+				startSlide();
+			}
+		 });
+		 startSlide(2000);
+
+
+	};
+
+	slider();
 
 });
